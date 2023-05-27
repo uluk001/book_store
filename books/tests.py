9b5efa4bc8e6fcc3dd56e85cdc9f4af9de176f3b
Hacker <Hacker@MacBook-Pro-2.local>
2023-05-27 21:20:34 +0600

    ch11

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,20 @@
-from django.test import TestCase 
-from django.urls import reverse 
-from .models import Book
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from .models import Book, Review
+
 
 class BookTests(TestCase): 
     def setUp(self):
-        self.book = Book.objects.create(title='Harry Potter', author='JK Rowling',price='25.00',)
-
-    def test_book_listing(self): 
+        self.user = get_user_model().objects.create_user(username='reviewuser', email='reviewuser@email.com',password='testpass123')
+        self.book = Book.objects.create(title='Harry Potter',author='JK Rowling', price='25.00',)
+        self.review = Review.objects.create(book = self.book,author = self.user, review = 'An excellent review',)
+    
+    def test_book_listing(self):   
         self.assertEqual(f'{self.book.title}', 'Harry Potter') 
         self.assertEqual(f'{self.book.author}', 'JK Rowling') 
         self.assertEqual(f'{self.book.price}', '25.00')
-
+    
     def test_book_list_view(self):
         response = self.client.get(reverse('book_list')) 
         self.assertEqual(response.status_code, 200) 
@@ -23,4 +27,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200) 
         self.assertEqual(no_response.status_code, 404) 
         self.assertContains(response, 'Harry Potter') 
+        self.assertContains(response, 'An excellent review')
         self.assertTemplateUsed(response, 'books/book_detail.html')
