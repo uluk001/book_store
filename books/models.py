fbceb9c3065ff01c66a877277adfbfff923909dc
Hacker <Hacker@MacBook-Pro-2.local>
2023-05-28 18:34:19 +0600

    ch13

--- books/models.py
+++ books/models.py
@@ -8,8 +8,11 @@ class Book(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6, decimal_places=2) 
-    cover = models.ImageField(upload_to='covers/', blank=True) 
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
+
+    class Meta: 
+        permissions = [('special_status', 'Can read all books'), ]
 
     def __str__(self):
         return self.title
--- books/views.py
+++ books/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-
-class BookListView(ListView): 
+class BookListView(LoginRequiredMixin, ListView): 
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    login_url = 'account_login'
 
     # def get_context_data(self, **kwargs):
     #     context = super(BookListView, self).get_context_data()
@@ -13,7 +14,9 @@ class BookListView(ListView):
     #     context['books'] = Book.objects.all()
     #     return context
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'
+    permission_required = 'books.special_status'
